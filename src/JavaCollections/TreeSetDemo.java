9be4e04d1383edb0b6cd471761fb836b6b72cc47
Muzafar12 <taranqi1214@gmail.com>
2020-04-30 18:11:49 -0400

    added new files

---
+++ src/JavaCollections/TreeSetDemo.java
@@ -0,0 +1,19 @@
+package JavaCollections;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+	public static void main(String[] args) {
+		
+		TreeSet<String> ts = new TreeSet<String>();
+		ts.add("33");
+		ts.add("44");
+		
+		Iterator<String> itr = ts.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+	}
+}
